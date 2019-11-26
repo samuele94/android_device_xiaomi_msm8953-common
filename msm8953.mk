a674f8bbea2003ba2bfcc543134db19904631695
Giovix92 <ggualtierone@gmail.com>
2019-11-26 13:39:35 -0500

    tissot: switch to soundtrigger 2.2

--- msm8953.mk
+++ msm8953.mk
@@ -84,10 +84,12 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.primary.msm8953 \
     audio.r_submix.default \
